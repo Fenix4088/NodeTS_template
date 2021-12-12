b4a066814710c46b475de599134c015c5bdf8939
Fenix4088 <fenix4088@gmail.com>
2021-12-12 16:26:09 +0200

    Started listen for a PORT in index file

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
+import dotenv from 'dotenv';
+import express from 'express';
+dotenv.config();
 
+const PORT: number = (process.env.PORT && +process.env.PORT) || 5000;
+
+const app = express();
+
+app.listen(PORT, () => {
+      console.log(`App started on PORT: ${PORT}`);
+});
