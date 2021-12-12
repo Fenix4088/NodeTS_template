39260ea88ec4f6b3980142ae41b8dfcaa42be487
Fenix4088 <fenix4088@gmail.com>
2021-12-12 23:32:21 +0200

    Mongoose added

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ const PORT: number = (process.env.PORT && +process.env.PORT) || 5000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).json('Server is working11!');
+});
+
 app.listen(PORT, () => {
-      console.log(`App started on PORT: ${PORT}`);
+  console.log(`App started on PORT: ${PORT}`);
 });
