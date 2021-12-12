2e885fd2bf150bf85d5bd9488d9d9b3461665079
Fenix4088 <fenix4088@gmail.com>
2021-12-12 23:48:07 +0200

    startApp added to index

--- index.ts
+++ index.ts
@@ -7,9 +7,17 @@ const PORT: number = (process.env.PORT && +process.env.PORT) || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
+  console.log(req.query);
+
   res.status(200).json('Server is working11!');
 });
 
-app.listen(PORT, () => {
-  console.log(`App started on PORT: ${PORT}`);
-});
+const startApp = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`App started on PORT: ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
